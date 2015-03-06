be357513d0fbdc2f963c889cc969ef09574f9013
Fred Souza <fredericoamsouza@gmail.com>
2015-03-06 01:06:16 -0300

    Intalled uglify plugin to use when producing build distro

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -4,10 +4,12 @@ var babel = require('gulp-babel');
 var paths = require('../paths');
 var compilerOptions = require('../6to5-options');
 var assign = Object.assign || require('object.assign');
+var uglify = require('gulp-uglify');
 
 gulp.task('build-source', function () {
     return gulp.src(paths.source)
         .pipe(babel(assign({}, compilerOptions, {modules:'common'}))) // modules can also be 'es6', 'amd' or 'system'
+        .pipe(uglify())
         .pipe(gulp.dest(paths.output));
 });
 
