9d7b294f09d9c38aeedadb9904bd91f3201ce2df
Fred Souza <fredericoamsouza@gmail.com>
2015-04-15 23:58:04 -0300

    Fixing Travis config

--- test/io.js
+++ test/io.js
@@ -2,9 +2,10 @@ let assert = require("assert");
 
 describe("Tests if can do IO nodejs operations", function() {
 
-    it("tests if can access node module", function() {
+    it("tests if can access node module", function(done) {
         "use strict";
         let fs = require('fs');
         assert.notEqual(Object.keys(fs).length, 0);
+        done();
     });
 });
--- test/search.js
+++ test/search.js
@@ -4,7 +4,7 @@ let assert = require('assert');
 
 describe("Search tests", function() {
 
-    it("tests search by line", function() {
+    it("tests search by line", function(done) {
         "use strict";
         let search1 = "485";
         let search2 = "663, 386";
@@ -29,9 +29,11 @@ describe("Search tests", function() {
 
         let buses5 = service.parseQueryData(search5, platformId);
         assert.equal(buses5.length, 0);
+
+        done();
     });
 
-    it("tests search by code", function() {
+    it("tests search by code", function(done) {
         "use strict";
         let search1 = "B31083";
         let search2 = "B31083, B31050";
@@ -52,9 +54,11 @@ describe("Search tests", function() {
 
         let buses4 = service.parseQueryData(search4, platformId);
         assert.equal(buses4.length, 0);
+
+        done();
     });
 
-    it("tests search by without line", function() {
+    it("tests search by without line", function(done) {
         "use strict";
         let search = "B71071";
 
@@ -63,5 +67,7 @@ describe("Search tests", function() {
 
         let buses = service.parseQueryData(search, platformId);
         assert(buses.length > 0);
+
+        done();
     });
 });
