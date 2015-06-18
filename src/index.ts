35f76ab332fc6c172090fdd3afc61f84a22dca78
Fred Souza <fredericoamsouza@gmail.com>
2015-06-17 21:00:15 -0300

    Refactoring index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 /// <reference path="../defs/tsd.d.ts" />
-import Application = require("./app");
-
 try{
-	Application.main(process.argv);
+	require("./app").main(process.argv);
 } catch(e){
 	console.log(e);
 }
