061b1855bbbc5980e4e25f0777f78bb878e38e43
Fred Souza <fredericoamsouza@gmail.com>
2015-06-24 23:09:48 -0300

    Removing sample test

--- test/sample.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="../defs/tsd.d.ts" />
-var Assert = require("assert");
-
-describe("Sample test case", () => {
-	it("should pass", (done) => {
-		Assert.equal(true, true);
-		done();
-	});
-});
