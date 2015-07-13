ae39596fe2d878b471e7e4705c0542e9b3e8ec16
Fred Souza <fredericoamsouza@gmail.com>
2015-07-12 23:07:06 -0300

    Limiting the search

--- src/business/searchBusiness.ts
+++ src/business/searchBusiness.ts
@@ -1,4 +1,5 @@
 import Bus 		   = require("../domain/entity/bus");
+import Config	   = require("../config");
 import IBusiness   = require("../business/iBusiness");
 import IDataAccess = require("../dataAccess/iDataAccess");
 import List 	   = require("../common/tools/list");
@@ -8,7 +9,10 @@ class SearchBusiness implements IBusiness {
 	
 	public constructor(private context: IDataAccess = $inject("dataAccess/searchDataAccess")) {}
 	
-	public retrieve(data?: string[]): Bus[] { 
+	public retrieve(data?: string[]): Bus[] {
+		if(data!==undefined && data.length>Config.maxSearchItems){
+			data = data.slice(0, Config.maxSearchItems);
+		} 
 		return this.context.retrieve(data);
 	}
 	
