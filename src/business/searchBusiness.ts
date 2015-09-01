b1d34d7eb09988342a1afbeafea025694d37cc07
lefreire <letfreirefigueiredo@gmail.com>
2015-09-01 19:04:05 -0300

    Renamed variable that represents if lines number need be limited or not

--- src/business/searchBusiness.ts
+++ src/business/searchBusiness.ts
@@ -23,14 +23,14 @@ class SearchBusiness implements IBusiness {
 	 * Retrieves the Bus data for a given line
 	 * @param {string} userAgent for track event in Google Analytics
 	 * @param {string} line
-	 * @param {boolean} consortiumFlag
+	 * @param {boolean} mustLimit
 	 * @return {Bus[]}
 	 */
-	public retrieve(userAgent: string, data?: string[], consortiumFlag?: boolean): Bus[] {
+	public retrieve(userAgent: string, data?: string[], mustLimit?: boolean): Bus[] {
 		var flag: any = Strings.analytics;
 		this.analytics.trackEvent(flag.event.restHit, flag.label.rest, userAgent, (error, response)=>{});
 		
-		if(data!==undefined && data.length>Config.maxSearchItems && consortiumFlag === false){
+		if(data!==undefined && data.length>Config.maxSearchItems && mustLimit === true){
 			data = data.slice(0, Config.maxSearchItems);
 		} 
 		this.analytics.trackEvent(flag.event.restHit, flag.label.busCode, data.join(","), (error, response)=>{});
