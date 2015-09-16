c2f9cf8f1d74f400decbf414f690cd79ba3845f4
lefreire <letfreirefigueiredo@gmail.com>
2015-09-16 20:01:02 -0300

    Fixing the test for itineraryService

--- test/service/itineraryService.ts
+++ test/service/itineraryService.ts
@@ -1,7 +1,4 @@
 declare var require, describe, it, global;
-import DbContext       = require("../../src/core/database/dbContext");
-import ICollection     = require("../../src/core/database/iCollection");
-import ItineraryModelMap = require("../../src/domain/modelMap/itineraryModelMap");
 import ItineraryService  = require("../../src/service/itineraryService");
 import IService        = require("../../src/service/iService");
 import Itinerary         = require("../../src/domain/entity/itinerary");
@@ -17,35 +14,9 @@ class MockedBusiness implements IBusiness{
 	public delete  (data: any): string { return data; }	
 }
 
-describe("ItineraryService", () => {
-	
-	var config: any = {
-		driver: "mongodb",
-		config: {
-			dbName: "riobus",
-			host: "ds047742.mongolab.com",
-			user: "riobus",
-			pass: "riobus",
-			port: "47742"
-		}
-	};
-	
-	var context: DbContext;
-	try{
-		context = new DbContext(config);
-	}catch(e){}
-	
-	
-	var collection: ICollection<Itinerary>;
-	try{
-		collection = <ICollection<Itinerary>> context.collection("itinerary", new ItineraryModelMap());
-	}
-	catch(e){}
-	
+describe("ItineraryService", () => {	
 	var itineraryService : IService = new ItineraryService(new MockedBusiness());
 	var itinerary : Itinerary = new Itinerary("line", "description", "agency", []);
-	
-	collection.save(itinerary);
 	
 		
 	it("should return a Itinerary object given a line", (done) => {
