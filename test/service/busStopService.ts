be345386db94a240bec2c3f640bdd00fcf9de294
lefreire <letfreirefigueiredo@gmail.com>
2015-09-16 19:59:26 -0300

    Fixing the test for busStopService

--- test/service/busStopService.ts
+++ test/service/busStopService.ts
@@ -1,7 +1,4 @@
 declare var require, describe, it, global;
-import DbContext       = require("../../src/core/database/dbContext");
-import ICollection     = require("../../src/core/database/iCollection");
-import BusStopModelMap = require("../../src/domain/modelMap/busStopModelMap");
 import BusStopService  = require("../../src/service/busStopService");
 import IService        = require("../../src/service/iService");
 import BusStop         = require("../../src/domain/entity/busStop");
@@ -19,32 +16,8 @@ class MockedBusiness implements IBusiness{
 
 describe("BusStopservice", () => {
 	
-	var config: any = {
-		driver: "mongodb",
-		config: {
-			dbName: "riobus",
-			host: "ds047742.mongolab.com",
-			user: "riobus",
-			pass: "riobus",
-			port: "47742"
-		}
-	};
-	
-	var context: DbContext;
-	try{
-		context = new DbContext(config);
-	}catch(e){}
-	
-	
-	var collection: ICollection<BusStop>;
-	try{
-		collection = <ICollection<BusStop>> context.collection("busStop", new BusStopModelMap());
-	}
-	catch(e){}
-	
 	var busService : IService = new BusStopService(new MockedBusiness());
 	var bus : BusStop = new BusStop("line", "description", "agency", []);
-	collection.save(bus);
 		
 	it("should return a BusStop object given a line", (done) => {
 		var current: boolean =  busService.retrieve(bus) instanceof BusStop;
