0268a967fe1b1a153b9c218f28b60b842cc771fa
lefreire <letfreirefigueiredo@gmail.com>
2015-09-21 19:18:15 -0300

    Added new field to itinerary constructor

--- src/domain/entity/itinerary.ts
+++ src/domain/entity/itinerary.ts
@@ -3,7 +3,7 @@ import ItinerarySpot = require("./itinerarySpot");
 class Itinerary {
 	
 	public constructor(private line: string, private description: string, 
-		private agency: string, private spots: ItinerarySpot[]) {}
+		private agency: string, private keywords: string, private spots: ItinerarySpot[]) {}
 	
 	public getLine(): string {
 		return this.line;
@@ -15,6 +15,10 @@ class Itinerary {
 	
 	public getAgency(): string{
 		return this.agency;
+	}
+	
+	public getKeywords(): string {
+		return this.keywords;
 	}
 	
 	public getSpots(): ItinerarySpot[] {
