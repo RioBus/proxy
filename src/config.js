5b00f50b3731d7918dc748b043be30351c5efc91
lefreire <letfreirefigueiredo@gmail.com>
2015-11-12 23:42:19 -0200

    Fixing log path

--- src/config.js
+++ src/config.js
@@ -1,4 +1,8 @@
+'use strict';
 /* global __dirname, process; */
+let data = new Date();
+let time = `${data.getFullYear()}-${data.getMonth()}-${data.getDate()}`;
+
 /**
  * Application configuration
  * You may use it to describe every global configuration data
@@ -6,8 +10,8 @@
 module.exports = {
     root: __dirname,
     logs: {
-        runtime: `/tmp/riobus/log/runtime-${new Date("Y-m-d").toISOString()}.log`,
-        server: `/tmp/riobus/log/server-${new Date("Y-m-d").toISOString()}.log`
+        runtime: `/tmp/riobus/log/runtime-${time}.log`,
+        server: `/tmp/riobus/log/server-${time}.log`
     },
     server: {
         ip: '0.0.0.0',
