34b54052e924aff25ac1c71ea4b32e6e43c3a6dd
lefreire <letfreirefigueiredo@gmail.com>
2015-11-16 21:15:08 -0200

    Making test for time on bus constructor

--- test/model/bus.js
+++ test/model/bus.js
@@ -1,17 +1,19 @@
 'use strict';
 /* global describe, it, before, global, __dirname, after; */
 const base = `${__dirname}/../../src`;
+const Moment = require('moment-timezone');
 
 const Assert = require('assert');
 const Database = require(`${base}/core`).Database;
 const Bus = require(`${base}/model/bus`);
 
-var dao, bus, col;
+var timestamp, bus, col;
 
 describe('Bus', () => {
 	
 	before(function() {
-		bus = new Bus('', 'order', '', '', 23, 45, (new Date()).toDateString(), '');
+		timestamp = (new Date()).toDateString();
+		bus = new Bus('', 'order', '', '', 23, 45, timestamp, '');
 	});
 	
 	it('should set line to \'indefinido\' when not given', function(done) {	
@@ -33,4 +35,12 @@ describe('Bus', () => {
 		Assert.equal(bus.sense, 'desconhecido');
 		done();
 	});
+	
+	it('should set the time with correct format', function*(done){
+		timestamp = (new Date(timestamp)).toISOString();
+		timestamp = Moment.tz(timestamp, "America/Sao_Paulo").format();
+		Assert.equal(bus.timestamp, timestamp);
+		done();
+	});
+	
 });
