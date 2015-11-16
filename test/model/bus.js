a64e1fb2865f0ad3d5854e9546827a69f00614d8
Fred Souza <fredericoamsouza@gmail.com>
2015-11-16 20:02:51 -0200

    Removing generators from Bus test - Reason: not async

--- test/model/bus.js
+++ test/model/bus.js
@@ -1,6 +1,5 @@
 'use strict';
 /* global describe, it, before, global, __dirname, after; */
-require('co-mocha')(require('mocha'));
 const base = `${__dirname}/../../src`;
 
 const Assert = require('assert');
@@ -11,31 +10,27 @@ var dao, bus, col;
 
 describe('Bus', () => {
 	
-	before(function*() {
+	before(function() {
 		bus = new Bus('', 'order', '', '', 23, 45, (new Date()).toDateString(), '');
 	});
 	
-	it('should set line to \'indefinido\' when not given', function*(done) {	
+	it('should set line to \'indefinido\' when not given', function(done) {	
 		Assert.equal(bus.line, 'indefinido');	
 		done();
 	});
 	
-	it('should set speed to 0 when not given', function*(done) {
+	it('should set speed to 0 when not given', function(done) {
 		Assert.equal(bus.speed, 0);
 		done();
 	});
 	
-	it('should set direction to 0 when not given', function*(done) {
+	it('should set direction to 0 when not given', function(done) {
 		Assert.equal(bus.direction, 0);
 		done();
 	});
 	
-	it('should set sense to \'desconhecido\' when not given', function*(done) {
+	it('should set sense to \'desconhecido\' when not given', function(done) {
 		Assert.equal(bus.sense, 'desconhecido');
 		done();
-	});
-
-		
-	after(function*() {
 	});
 });
