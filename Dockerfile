5aa26c8d874a605cbd182d8381d999db6eb42fdb
Fred Souza <fredericoamsouza@gmail.com>
2015-11-19 15:56:49 -0200

    Removing Docker config files

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM node:latest
-EXPOSE 8080
-EXPOSE 80
-RUN npm install -g mocha
-RUN mkdir /app
-WORKDIR /app
--- env.config.sh
+++
@@ -1,8 +0,0 @@
-export RIOBUS_DB_NAME="riobus";
-export RIOBUS_DB_HOST="localhost";
-export RIOBUS_DB_USER="";
-export RIOBUS_DB_PASS="";
-export RIOBUS_DB_PORT="27017";
-export RIOBUS_DB_DRIVER="mongodb";
-export RIOBUS_ANALYTICS_UA="";
-export RIOBUS_ANALYTICS_HOST="";
