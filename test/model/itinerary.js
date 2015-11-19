0ac485495eb45098986feedea21967064e4add44
Fred Souza <fredericoamsouza@gmail.com>
2015-11-19 17:33:27 -0200

    Fixing Itinerary model path

--- test/model/itinerary.js
+++ test/model/itinerary.js
@@ -4,7 +4,7 @@
 const Assert = require('assert');
 
 const base = '../../src';
-const Itinerary = require(`${base}/model/itinerary`);
+const Itinerary = require(`${base}/itinerary/itineraryModel`);
 
 describe('Itinerary', () => {
 	
