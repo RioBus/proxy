056d48b6616f7f1568c753ea501039a35dd517b7
Fred Souza <fredericoamsouza@gmail.com>
2015-12-07 00:11:45 -0200

    Testing the GET request to an unknown line

--- test/api/bus.js
+++ test/api/bus.js
@@ -18,28 +18,41 @@ describe('Bus API', () => {
 		host = `http://${ip}:${port}`;
 		
 		global.database = yield Database.connect();
-		yield global.database.collection('bus').insert(new Bus('line', 'order', 0, 0, 20, 30, (new Date()).toDateString(), 'sense'));
+		yield global.database.collection('bus').insert(new Bus('485', 'order', 0, 0, 20, 30, (new Date()).toDateString(), 'sense'));
 		
 		let router = new Router();
 		router.registerResources(['bus/busResource']);
 		server = router.start(ip, port);
 	});
 	
-	it('should get a list with all bus from GET request by v3/search/data', function*() {
+	it('should get a list of all buses from a GET request to /v3/search/485', function*() {
 		let data;
 		try {
-			var output = yield Http.get(`${host}/v3/search/line`);
+			var output = yield Http.get(`${host}/v3/search/485`);
 			data = JSON.parse(output);
 		} catch(e) {
 			data = e;
 		} finally {
 			Assert.equal(data instanceof Array, true);
 			Assert.equal(data.length, 1);
-			Assert.equal(data[0].line, 'line');
+			Assert.equal(data[0].line, '485');
 			Assert.equal(data[0].order, 'order');
 			Assert.equal(data[0].speed, 0);
 			Assert.equal(data[0].direction, 0);
 			Assert.equal(data[0].sense, 'sense');
+		}
+	});
+	
+	it('should get an empty list from a GET request to v3/search/unexisting', function*() {
+		let data;
+		try {
+			var output = yield Http.get(`${host}/v3/search/unexisting`);
+			data = JSON.parse(output);
+		} catch(e) {
+			data = e;
+		} finally {
+			Assert.equal(data instanceof Array, true);
+			Assert.equal(data.length, 0);
 		}
 	});
 	
