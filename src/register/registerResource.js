4490f7a4f4aeff575c9208159544eece77100a4f
igor vaz <ivaz567@hotmail.com>
2016-03-20 12:47:06 -0300

    closes #33

--- src/register/registerResource.js
+++ src/register/registerResource.js
@@ -3,7 +3,7 @@ const wrap = require('co-express');
 const Core = require('../core');
 const Database = Core.Database;
 const Register = require('./registerModel');
-const RegisterDAO = require('./registerDAO')
+const RegisterDAO = require('./registerDAO');
 const Security = require('../common/securityUtil');
 
 class RegisterResource {
@@ -24,14 +24,14 @@ class RegisterResource {
 			if(typeof(request.body.password)!=='string' || request.body.password.lenght == 0) throw new Error("password is invalid");
 			if(request.body.profile_image !== undefined && request.body.profile_image !== "") aux.profile_image = request.body.profile_image;
 			if(request.body.social !== undefined){
-				if (request.body.social.facebbok_id !== undefined) {aux.social.facebbok_id = request.body.social.facebbok_id};
-				if (request.body.social.google_id !== undefined) {aux.social.google_id = request.body.social.google_id};
-				if (request.body.social.twitter_id !== undefined) {aux.social.twitter_id = request.body.social.twitter_id};
+				if (request.body.social.facebook_id !== undefined) {aux.social.facebook_id = request.body.social.facebook_id;}
+				if (request.body.social.google_id !== undefined) {aux.social.google_id = request.body.social.google_id;}
+				if (request.body.social.twitter_id !== undefined) {aux.social.twitter_id = request.body.social.twitter_id;}
 			}
 			var user = new Register(request.body.name,request.body.email,request.body.password,aux.profile_image,aux.social);
 			yield dao.save(user);
 			let data = {
-				auth_token: yield Security.generateToken(user.email);
+				auth_token: yield Security.generateToken(user.email),
 				data:{
 					name:user.name,
 					email:user.email
